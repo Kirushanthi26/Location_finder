a1f16099db43448ea0ddcef70a43bd460b28ec9c
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-12-04 17:18:51 +0530

    navlink

--- react-frontend-01-starting-setup/src/shared/components/Navigation/MainNavigation.js
+++ react-frontend-01-starting-setup/src/shared/components/Navigation/MainNavigation.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import NavLinks from "./NavLinks";
 
 const MainNavigation = (props) => {
   return (
@@ -13,9 +14,9 @@ const MainNavigation = (props) => {
       </button>
       <h1 className="main-navigation__title">
         <Link to="/">YourPlaces</Link>
-        </h1>
+      </h1>
       <nav>
-        ...
+        <NavLinks />
       </nav>
     </MainHeader>
   );
