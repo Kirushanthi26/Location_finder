4aa64f9ee40880f42d5b58b6dd51036b54e5469d
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-12-06 12:12:39 +0530

    mobile view side view, portals

--- react-frontend-01-starting-setup/src/shared/components/Navigation/SideDrawer.js
+++ react-frontend-01-starting-setup/src/shared/components/Navigation/SideDrawer.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './SideDrawer.css'
+import React from 'react';
+import ReactDOM  from 'react-dom';
 
-const SideDrawer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import './SideDrawer.css';
 
-export default SideDrawer
+const SideDrawer = props => {
+  const content =  <aside className="side-drawer">{props.children}</aside>;
+
+  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
+};
+
+export default SideDrawer;
