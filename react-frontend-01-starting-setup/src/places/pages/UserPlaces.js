26a000ec613f39d03d3d58a082f44c4630455862
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-12-06 14:02:12 +0530

    display dummy place for specif user and route

--- react-frontend-01-starting-setup/src/places/pages/UserPlaces.js
+++ react-frontend-01-starting-setup/src/places/pages/UserPlaces.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from "react";
+import PlaceList from "../components/PlaceList";
 
-//one user -> places list 
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Lotus Tower",
+    description:
+      "Lotus Tower located in Colombo, Sri Lanka. It has been called a symbolic landmark of Sri Lanka. ",
+    imageUrl:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/61/14/af/caption.jpg?w=1200&h=1200&s=1",
+    address: "AC6, Colombo 01000",
+    location: {
+      lat: 6.9273044588293065,
+      lng: 79.8583383153592,
+    },
+    creator:'u1'
+  },
+  {
+    id: "p2",
+    title: "Lotus Tower",
+    description:
+      "Lotus Tower located in Colombo, Sri Lanka. It has been called a symbolic landmark of Sri Lanka. ",
+    imageUrl:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/61/14/af/caption.jpg?w=1200&h=1200&s=1",
+    address: "AC6, Colombo 01000",
+    location: {
+      lat: 6.9273044588293065,
+      lng: 79.8583383153592,
+    },
+    creator:'u2'
+  },
+];
+
+//one user -> places list
 const UserPlaces = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <PlaceList items={DUMMY_PLACES} />;
+};
 
-export default UserPlaces
+export default UserPlaces;
