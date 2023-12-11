d45c5f0dd00d52f83d7be29519784335fd83fcbe
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-12-11 15:14:47 +0530

    description input added

--- react-frontend-01-starting-setup/src/places/pages/NewPlace.js
+++ react-frontend-01-starting-setup/src/places/pages/NewPlace.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useCallback } from "react";
 import "./NewPlace.css";
 import Input from "../../shared/components/FormElements/Input";
-import {VALIDATOR_REQUIRE} from '../../shared/util/validators'
+import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from '../../shared/util/validators'
 
 const NewPlace = () => {
+
+  const titleInputHandler = useCallback((id, value, isValid) => {
+
+  },[])
+  const descriptionInputHandler = useCallback((id, value, isValid) => {
+
+  },[])
+
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         type="text"
         label="Title"
         element="input"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        label="Description"
+        element="textarea"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={descriptionInputHandler}
       />
     </form>
   );
