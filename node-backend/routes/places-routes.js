22a3b4e4c4dbc493e859d63e21087237240e399b
kirushanthi26 <kirushanthikiru26@gmail.com>
2024-01-16 17:14:25 +0530

    handling error -> next or throw

--- node-backend/routes/places-routes.js
+++ node-backend/routes/places-routes.js
@@ -1,62 +1,68 @@
-const express = require("express")
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
 const DUMMY_PLACES = [
-    {
-      id: "p1",
-      title: "Lotus Tower",
-      description:
-        "Lotus Tower located in Colombo, Sri Lanka. It has been called a symbolic landmark of Sri Lanka. ",
-      imageUrl:
-        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/61/14/af/caption.jpg?w=1200&h=1200&s=1",
-      address: "AC6, Colombo 01000",
-      location: {
-        lat: 6.9273044588293065,
-        lng: 79.8583383153592,
-      },
-      creator:'u1'
+  {
+    id: "p1",
+    title: "Lotus Tower",
+    description:
+      "Lotus Tower located in Colombo, Sri Lanka. It has been called a symbolic landmark of Sri Lanka. ",
+    imageUrl:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/61/14/af/caption.jpg?w=1200&h=1200&s=1",
+    address: "AC6, Colombo 01000",
+    location: {
+      lat: 6.9273044588293065,
+      lng: 79.8583383153592,
     },
-    {
-      id: "p2",
-      title: "Lotus Tower",
-      description:
-        "Lotus Tower located in Colombo, Sri Lanka. It has been called a symbolic landmark of Sri Lanka. ",
-      imageUrl:
-        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/61/14/af/caption.jpg?w=1200&h=1200&s=1",
-      address: "AC6, Colombo 01000",
-      location: {
-        lat: 6.9273044588293065,
-        lng: 79.8583383153592,
-      },
-      creator:'u2'
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Lotus Tower",
+    description:
+      "Lotus Tower located in Colombo, Sri Lanka. It has been called a symbolic landmark of Sri Lanka. ",
+    imageUrl:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/61/14/af/caption.jpg?w=1200&h=1200&s=1",
+    address: "AC6, Colombo 01000",
+    location: {
+      lat: 6.9273044588293065,
+      lng: 79.8583383153592,
     },
-  ];
+    creator: "u2",
+  },
+];
 
-router.get('/:pid', (req, res, next)=>{
-    const placeID = req.params.pid;
-    const place = DUMMY_PLACES.find((p)=>{
-        return p.id === placeID
-    })
+router.get("/:pid", (req, res, next) => {
+  const placeID = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeID;
+  });
 
-    if (!place) {
-       return res.status(404).json({message: "Could not find a place for the provided id."})    
-    }
+  if (!place) {
+    //return res.status(404).json({message: "Could not find a place for the provided id."})
+    const error = new Error("Could not find a place for the provided id.");
+    error.code = 404;
+    throw error;
+  }
 
-    res.json({place})
-})
+  res.json({ place });
+});
 
-router.get('/user/:uid', (req, res, next) => {
-    const userID = req.params.uid;
-    const place = DUMMY_PLACES.find( (p) => {
-        return p.creator === userID
-    })
+router.get("/user/:uid", (req, res, next) => {
+  const userID = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userID;
+  });
 
-    if (!place) {
-        return res.status(404).json({message: "Could not find a place for the provided user id."})    
-     }
+  if (!place) {
+    //return res.status(404).json({message: "Could not find a place for the provided user id."})
+    const error = new Error("Could not find a place for the provided user id.");
+    error.code = 404;
+    return next(error);
+  }
 
-    res.json({place})
-})
+  res.json({ place });
+});
 
 module.exports = router;
