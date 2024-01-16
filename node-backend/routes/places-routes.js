0c9cd512379b0225eee907426528650550b3c34c
kirushanthi26 <kirushanthikiru26@gmail.com>
2024-01-16 17:26:05 +0530

    error model

--- node-backend/routes/places-routes.js
+++ node-backend/routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error")
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -41,8 +43,7 @@ router.get("/:pid", (req, res, next) => {
 
   if (!place) {
     //return res.status(404).json({message: "Could not find a place for the provided id."})
-    const error = new Error("Could not find a place for the provided id.");
-    error.code = 404;
+    const error = new HttpError("Could not find a place for the provided id.", 404);
     throw error;
   }
 
@@ -57,9 +58,7 @@ router.get("/user/:uid", (req, res, next) => {
 
   if (!place) {
     //return res.status(404).json({message: "Could not find a place for the provided user id."})
-    const error = new Error("Could not find a place for the provided user id.");
-    error.code = 404;
-    return next(error);
+    return next(new HttpError("Could not find a place for the provided user id.", 404));
   }
 
   res.json({ place });
