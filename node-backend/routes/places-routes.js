b61732143539ace60fbb8e2dc063da6276e85405
kirushanthi26 <kirushanthikiru26@gmail.com>
2024-01-16 16:16:03 +0530

    comman path setup

--- node-backend/routes/places-routes.js
+++ node-backend/routes/places-routes.js
@@ -2,6 +2,37 @@ const express = require("express")
 
 const router = express.Router()
 
+const DUMMY_PLACES = [
+    {
+      id: "p1",
+      title: "Lotus Tower",
+      description:
+        "Lotus Tower located in Colombo, Sri Lanka. It has been called a symbolic landmark of Sri Lanka. ",
+      imageUrl:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/61/14/af/caption.jpg?w=1200&h=1200&s=1",
+      address: "AC6, Colombo 01000",
+      location: {
+        lat: 6.9273044588293065,
+        lng: 79.8583383153592,
+      },
+      creator:'u1'
+    },
+    {
+      id: "p2",
+      title: "Lotus Tower",
+      description:
+        "Lotus Tower located in Colombo, Sri Lanka. It has been called a symbolic landmark of Sri Lanka. ",
+      imageUrl:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/19/61/14/af/caption.jpg?w=1200&h=1200&s=1",
+      address: "AC6, Colombo 01000",
+      location: {
+        lat: 6.9273044588293065,
+        lng: 79.8583383153592,
+      },
+      creator:'u2'
+    },
+  ];
+
 router.get('/', (req, res, next)=>{
     console.log("it's working - places")
     res.json({message:"get req is working - places"})
