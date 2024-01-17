671f7eaba3ab9c4d50d73de28ddd87dae43eeca1
kirushanthi26 <kirushanthikiru26@gmail.com>
2024-01-17 14:22:50 +0530

    users validation

--- node-backend/controller/users-controller.js
+++ node-backend/controller/users-controller.js
@@ -1,18 +1,19 @@
 const { v4: uuidv4 } = require("uuid");
 const HttpError = require("../models/http-error");
+const { validationResult } = require("express-validator");
 
 let DUMMY_USERS = [
   {
     id: "u1",
     name: "kirushanthi",
-    email:"kirushanthi@gmail.com",
-    password:"kiru123"
+    email: "kirushanthi@gmail.com",
+    password: "kiru123",
   },
   {
     id: "u2",
     name: "vithya",
-    email:"vithya@gmail.com",
-    password:"vithya123"
+    email: "vithya@gmail.com",
+    password: "vithya123",
   },
 ];
 
@@ -20,41 +21,51 @@ const getAllUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 
   if (DUMMY_USERS.length === 0) {
-    return next(new HttpError("could not find users", 404))
+    return next(new HttpError("could not find users", 404));
   }
 };
 
 const createNewUser = (req, res, next) => {
-    const {name, email, password} = req.body;
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    throw new HttpError("invalid input", 422);
+  }
 
-    const hasUser = DUMMY_USERS.find(u => u.email === email)
-    if (hasUser) {
-        throw new HttpError("already registered, please Login", 401)
-    }
+  const { name, email, password } = req.body;
 
-    const signupUser = {
-        id: uuidv4(),
-        name,
-        email,
-        password
-    }
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  if (hasUser) {
+    throw new HttpError("already registered, please Login", 401);
+  }
 
-    DUMMY_USERS.push(signupUser)
-    res.status(201).json({user: signupUser})
-}
+  const signupUser = {
+    id: uuidv4(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(signupUser);
+  res.status(201).json({ user: signupUser });
+};
 
 const loginUser = (req, res, next) => {
-    const {email, password} = req.body;
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    throw new HttpError("invalid input", 422);
+  }
+  
+  const { email, password } = req.body;
 
-    const identifiedUser = DUMMY_USERS.find(u => u.email === email)
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
 
-    if (!identifiedUser || identifiedUser.password !== password) {
-        throw new HttpError("wrong credentials, please try again", 401)
-    }
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError("wrong credentials, please try again", 401);
+  }
 
-    res.json({message: 'Login successful'})
-}
+  res.json({ message: "Login successful" });
+};
 
 exports.getAllUsers = getAllUsers;
-exports.createNewUser = createNewUser
-exports.loginUser = loginUser
+exports.createNewUser = createNewUser;
+exports.loginUser = loginUser;
--- node-backend/routes/users-routes.js
+++ node-backend/routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const router = express.Router();
 
@@ -6,8 +7,23 @@ const usersController = require("../controller/users-controller");
 
 router.get("/", usersController.getAllUsers);
 
-router.post("/signup", usersController.createNewUser)
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  usersController.createNewUser
+);
 
-router.post("/login", usersController.loginUser )
+router.post(
+  "/login",
+  [
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  usersController.loginUser
+);
 
 module.exports = router;
